8a1ab26d9df9804d4747cfca5da61c44a70fec1a
A. Filippow <a.filippow@qtech.ru>
2024-01-11 23:36:11 +0900

    UI optimization

--- pages/fiscal/upload.vue
+++ pages/fiscal/upload.vue
@@ -14,12 +14,13 @@ async function upload(e: any) {
 
 <template lang="pug">
 input(type="file" ref="input" @change="upload" accept=".json" hidden)
-v-btn(@click="()=>input.click()" :loading="loading") Выбрать JSON файл отправленный приложением
+v-btn(@click="()=>input.click()" :loading="loading" color="primary" ) Выбрать JSON файл отправленный приложением
 v-divider.ma-3
 
 a(href='https://play.google.com/store/apps/details?id=ru.fns.billchecker&hl=ru&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1' target="_blank")
-    img( alt='Доступно в Google Play' src='https://raw.githubusercontent.com/steverichey/google-play-badge-svg/266d2b2df26f10d3c00b8129a0bd9f6da6b19f00/img/ru_get.svg' width="200")
     div Приложение "Проверка чеков ФНС России"
+    img( alt='Доступно в Google Play' src='https://raw.githubusercontent.com/steverichey/google-play-badge-svg/266d2b2df26f10d3c00b8129a0bd9f6da6b19f00/img/ru_get.svg' width="200")
+
 </template>
 
 <style scoped>
